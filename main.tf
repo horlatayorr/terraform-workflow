d2486c78e988ad495f30bc6ec9e75688b3d01b78
Olatayo Adewuyi <adewuyiifs172340@futa.edu.ng>
2024-06-27 19:04:06 +0100

    AWS Configuration Update

--- main.tf
+++ main.tf
@@ -16,8 +16,8 @@ resource "aws_instance" "example" {
   ami                    = "ami-0c7217cdde317cfec"
   instance_type          = "t2.micro"
   key_name               = "terraform-aws"
-  vpc_security_group_ids = ["sg-0c51540c60857b7ed", "sg-0c51540c60857b7ed"]
-  subnet_id              = "subnet-096d45c28d9fb4c14"
+  vpc_security_group_ids = ["sg-0ac806611b57b8c37", "sg-0ac806611b57b8c37"]
+  subnet_id              = "subnet-071688abd35541481"
   root_block_device {
     volume_size = "10"
   }
